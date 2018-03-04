d64278aec0c407adc75c189f92055359816172a8
ariel wilson <ariel.wilson@mymona.uwi.edu>
2018-03-04 19:42:45 +0000

    step 8 complete

--- app/views.py
+++ app/views.py
@@ -31,23 +31,37 @@ def about():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
-    if request.method == "POST":
+    #if request.method == "POST":
         # change this to actually validate the entire form submission
         # and not just one field
-        if form.username.data:
+    if request.method == "POST" and form.validate_on_submit():
+        if form.username.data and form.password.data:
             # Get the username and password values from the form.
-
+            username = form.username.data
+            password = form.password.data
             # using your model, query database for a user based on the username
             # and password submitted
             # store the result of that query to a `user` variable so it can be
+            user = UserProfile.query.filter_by(username=username, password=password).first()
             # passed to the login_user() method.
-
+            if user is not None:
+                remember_me = False
+                
+                if 'remember_me' in request.form:
+    
+                    remember_me = True
+            
             # get user id, load into session
-            login_user(user)
-
-            # remember to flash a message to the user
-            return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
+                login_user(user, remember=remember_me)
+                flash('Logged in successfully.', 'success')
+                next_page = request.args.get('next')
+                
+                return redirect(next_page or url_for('secure_page'))  # they should be redirected to a secure-page route instead
+            else:
+                # remember to flash a message to the user
+                flash('Username or Password is incorrect.', 'danger')
     return render_template("login.html", form=form)
+
 
 
 # user_loader callback. This callback is used to reload the user object from
